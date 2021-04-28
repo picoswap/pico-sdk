2b1780d70f7220b54448d9bfa908f9adac58e85a
Raymond Zhong <raykyri@gmail.com>
2021-04-27 20:37:57 -0700

    Add core-sdk exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import JSBI from 'jsbi'
 export { JSBI }
 
+export {
+  ChainId,
+  Currency,
+  CurrencyAmount,
+  currencyEquals,
+  ETHER,
+  Fraction,
+  Percent,
+  Price,
+  sortedInsert,
+  Token,
+  TokenAmount,
+  TradeType,
+  WETH9
+} from '@picoswap/sdk-core'
+
 export { FACTORY_ADDRESS, INIT_CODE_HASH, MINIMUM_LIQUIDITY } from './constants'
 
 export * from './errors'
